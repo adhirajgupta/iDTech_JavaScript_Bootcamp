65fe2c1faea9720831e4de96f45990d9d1e616ee
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2022-04-04 17:09:47 +0530

    Add files via upload

---
+++ Fortune Teller Activity/fortune_teller.js
@@ -0,0 +1,20 @@
+// TODO: Create a variable called "fortune" and assign a number between 0 - 10.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create a fortune teller game using conditional statements and comparison operators.
+// Conditions
+// 1. If fortune is greater than or equal to 0 and less than or equal to 3, then you have a low fortune.
+// 2. If fortune is greater than 3 and less than or equal to 7, then you have an average fortune.
+// 3. If fortune is greater than 7 and less than or equal to 10, then you have a good fortune.
+// 4. If the fortune is out of range, then the fortune can't be read correctly.
+
+// YOUR CODE GOES HERE
+
+let fortune = 8
+console.log("fortune selected:",fortune)
+
+if(fortune>=0&&fortune<=3)console.log("Oh no! You selected a low fortune")
+else if (fortune>=4&&fortune<=7)console.log("You selected a average fortune")
+else if (fortune>=8&&fortune<=10)console.log("Congratulations! You selected a great fortune")
+else console.log("It was hard to determine your fortune, try again!")
