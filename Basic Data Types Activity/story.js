ce3e8f2f9d4c1ab22feba5cb36ab2d34a93d8939
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2022-04-04 17:10:34 +0530

    Add files via upload

---
+++ Basic Data Types Activity/story.js
@@ -0,0 +1,20 @@
+let greeting = "Welcome to"
+let country = "India"
+let title = "Beauties of India"
+let ContinentName = "Asia"
+
+let timezone = 82_30;
+let states = 29
+let union_territories = 9;
+
+let space_achievements = ["Mars","Moon","Earth"]
+
+let nuclear_country = true
+
+
+console.log(
+    greeting,country,"a country who is a part of",ContinentName,".Today I am going to show the",title,
+)
+console.log("It lies in the timezone",timezone,"with",states,"states","and",union_territories,"union territories")
+console.log("Its major space achievements include",space_achievements[0],space_achievements[1],space_achievements[2])
+console.log("It is a united,deadly and it is",nuclear_country,"that it is a nucleur country")
